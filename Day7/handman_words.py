d4b9bf44a2852fb1735a44c0affcd7a1a29cb746
Carlos Villegas <cvillegasfrola@gmail.com>
2021-07-23 14:32:04 -0500

    Day7 Python Bootcamp

---
+++ Day7/handman_words.py
@@ -0,0 +1,14 @@
+word_list = [
+    "mexico",
+    "aguacate",
+    "popocatepetl",
+    "guadalajara",
+    "automovil",
+    "azteca",
+    "unicornio",
+    "cancion",
+    "guitarra",
+    "camello",
+    "jupiter",
+    "caballo"
+]
