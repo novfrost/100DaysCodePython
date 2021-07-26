99295e5ced42c4eb8a41eb7f03638f10dc90c50e
Carlos Villegas <cvillegasfrola@gmail.com>
2021-07-26 17:14:51 -0500

    Day 10 Python BootCamp

---
+++ Day10/functions.py
@@ -0,0 +1,12 @@
+# Functions with outputs
+
+def format_name(f_name, l_name):
+    '''
+    This is a function that title the name
+    This is a docstring
+    '''
+    return f"{f_name.title()} {l_name.title()}"
+
+#print(format_name("carlos","villegas"))
+
+
